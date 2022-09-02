f5e4efb518b18ac36e8550ea1ee3490b6ea3844d
chai <173985495@qq.com>
2022-09-02 22:04:04 +0800

    添加内容

---
+++ demo/ogcRelation.js
@@ -0,0 +1,31 @@
+/**
+ * Created by zhaoy on 2022/9/2.
+ */
+const nodeDataArray = [
+    {key: 1, text: "geos"},
+    {key: 2, text: "postgis"},
+    {key: 3, text: "qgis"},
+    {key: 4, text: "gdal"},
+    {key: 5, text: "shapely"},
+    {key: 6, text: "jts"},
+    {key: 7, text: "geotols"},
+    {key: 8, text: "NetTopology\nSuite"},
+    {key: 9, text: "JSTS"},
+    {key: 10, text: "R\nGEOS"},
+    {key: 11, text: "rgeo"},
+    {key: 12, text: "GEO\nSwift"}
+];
+const linkDataArray = [
+    {from: 1, to: 2, text: "空间功能"},
+    {from: 1, to: 3, text: "空间功能"},
+    {from: 1, to: 4, text: "空间功能"},
+    {from: 1, to: 5, text: "空间功能"},
+    {from: 6, to: 7, text: "空间功能"},
+    {from:6,to:1,text:"移植"},
+    {from:6,to:8,text:"移植"},
+    {from:6,to:9,text:"移植"},
+    {from:1,to:10,text:"空间功能"},
+    {from:1,to:11,text:"空间功能"},
+    {from:1,to:12,text:"空间功能"}
+
+];
